4d2e487a89dd813e7ac9f004a738b8aab430ffa4
Sandeep Thakare <sandeepthakare@Sandeeps-MacBook-Air.local>
2025-07-05 21:30:19 +0530

    Valdator test cased added to handle negetive numbers, expect test cases to failed - Code update WIP

--- src/validators/numberValidators.js
+++ src/validators/numberValidators.js
@@ -1,13 +1,33 @@
+// src/validators/NumberValidator.js
+
 const { MESSAGES } = require('../utils');
 
 class NumberValidator {
+  /**
+   * Validates an array of numbers
+   * @param {number[]} numbers - Array of numbers to validate
+   * @throws {Error} - Throws error if validation fails
+   */
   validate(numbers) {
-    const negativeNumbers = numbers.filter(num => num < 0);
-    
-    if (negativeNumbers.length > 0) {
-      const message = `${MESSAGES.NEGATIVE_NOT_ALLOWED} ${negativeNumbers.join(',')}`;
-      throw new Error(message);
-    }
+    this.validateNegativeNumbers(numbers);
+  }
+  
+  /**
+   * Validates that no negative numbers are present
+   * @param {number[]} numbers - Array of numbers to check
+   * @throws {Error} - Throws error if negative numbers found
+   */
+  validateNegativeNumbers(numbers) {
+    throw new Error('Not implemented');
+  }
+  
+  /**
+   * Finds all negative numbers in the array
+   * @param {number[]} numbers - Array of numbers to check
+   * @returns {number[]} - Array of negative numbers
+   */
+  findNegativeNumbers(numbers) {
+    return numbers.filter(num => num < 0);
   }
 }
 
--- tests/unit/validators/numberValidator.test.js
+++ tests/unit/validators/numberValidator.test.js
@@ -28,4 +28,31 @@ describe('NumberValidator', () => {
       expect(() => validator.validate([])).not.toThrow();
     });
   });
+
+  describe('validateNegativeNumbers method', () => {
+    test('should not throw for positive numbers', () => {
+      expect(() => validator.validateNegativeNumbers([1, 2, 3])).not.toThrow();
+    });
+
+    test('should throw for negative numbers', () => {
+      expect(() => validator.validateNegativeNumbers([-1])).toThrow();
+      expect(() => validator.validateNegativeNumbers([-1, -2])).toThrow();
+    });
+  });
+
+  describe('findNegativeNumbers method', () => {
+    test('should return empty array for positive numbers', () => {
+      expect(validator.findNegativeNumbers([1, 2, 3])).toEqual([]);
+    });
+
+    test('should return negative numbers', () => {
+      expect(validator.findNegativeNumbers([-1, 2, -3])).toEqual([-1, -3]);
+      expect(validator.findNegativeNumbers([-5, -10, -15])).toEqual([-5, -10, -15]);
+    });
+
+    test('should handle zero', () => {
+      expect(validator.findNegativeNumbers([0, 1, 2])).toEqual([]);
+      expect(validator.findNegativeNumbers([-1, 0, 1])).toEqual([-1]);
+    });
+  });
 });

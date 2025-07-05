a91e54050641faebd27b7e34bfee9d9a1131fe4e
Sandeep Thakare <sandeepthakare@Sandeeps-MacBook-Air.local>
2025-07-05 21:41:29 +0530

    added missed test cases for parser

--- tests/unit/parsers/numberParser.test.js
+++ tests/unit/parsers/numberParser.test.js
@@ -14,11 +14,27 @@ describe('NumberParser', () => {
     parser = new NumberParser();
   });
 
+  test('should return empty array for empty string', () => {
+    expect(parser.parse('', ',')).toEqual([]);
+  });
+
+  test('should parse single number', () => {
+    expect(parser.parse('5', ',')).toEqual([5]);
+  });
+
   test('should parse comma-separated numbers', () => {
     expect(parser.parse('1,2,3', ',')).toEqual([1, 2, 3]);
   });
 
+  test('should parse numbers with custom delimiter', () => {
+    expect(parser.parse('1;2;3', ';')).toEqual([1, 2, 3]);
+  });
+
   test('should handle newlines mixed with delimiters', () => {
     expect(parser.parse('1\n2,3', ',')).toEqual([1, 2, 3]);
+  });
+
+  test('should handle negative numbers', () => {
+    expect(parser.parse('-1,2,-3', ',')).toEqual([-1, 2, -3]);
   });
 });
